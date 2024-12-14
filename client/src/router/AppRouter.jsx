34e8c371bf7ea359d19515073ce70e841c7268b7
Sibanijana <janaasibani@gmail.com>
2024-12-15 02:22:07 +0530

    register page

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import AdminPage from "../pages/AdminPage";
 //import DepartmentPage from "../pages/DepartmentPage";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../components/auth/LoginPage";
+import RegisterPage from "../components/auth/RegisterPage";
 
 const AppRouter = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/admin" element={<AdminPage />} />
           {/* <Route
             path="/department/:department_name"
